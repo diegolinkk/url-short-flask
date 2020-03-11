eafdc5349f0e3809d7241caf1ac80d1958503414
Diego Oliveira da Silva <diegolinkk@gmail.com>
2020-03-10 23:21:34 -0300

    esqueleto básico, organização do backlog e adicionando readme

--- app/app.py
+++ app/app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+#renderiza form e permite o cadastro
+@app.route('/', methods=['GET','POST'])
 def index():
-    return "Hellor, world"
+    return render_template("base.html")
+
+
+#recebe a url,procura na base e encaminha
+#tratar os erros caso a url não existir
+@app.route('/<url>')
+def encaminhar(url):
+    return f"A url digitada foi {url}"
